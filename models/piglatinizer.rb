c52557553571b6710a54519073d929e5ec5eae1a
Taylor Treece <taylor.treece@protonmail.com>
2021-03-09 08:04:21 -0600

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,29 +1,23 @@
 class PigLatinizer
 
     def initialize
-   
     end
 
     def piglatinize(words)
-      vowels=['a', 'A', 'e', 'E' 'i', 'I', 'o', 'O', 'u', 'U', 'y', 'Y']
+      vowels=['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'y', 'Y']
 
-      @answer=words.split(" ").map do |word|
-      if vowels.include?(word[0])
+        words.split(" ").map do |word|
+      if vowels.include?(word[0]) || word.length == 1
         word+"way"
       else
-        a='' 
+        consonants='' 
         letters=word.split("")
-        until vowels.include?(letters[0]) do
-          a<<letters.shift
+        until vowels.include?(letters[0]) || letters.length <= 0 do
+          consonants<<letters.shift
         end
-          letters.join("") + a + "ay"
+          letters.join("") + consonants + "ay"
         end
-      end.join(" ").to_s
-      @answer
+      end.join(" ")
     end
-
-    # def variable
-    #     @words=piglatinize
-    # end
 
 end
